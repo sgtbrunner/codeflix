d154ee483092aae2a37930df2bae307eb8434402
Guilherme Brunner <guilherme.brunner@gmail.com>
2020-08-12 21:22:39 -0300

    small improvements

--- src/services/dataStorage.service.js
+++ src/services/dataStorage.service.js
@@ -1,32 +1,27 @@
 import { URL_CONSTANTS } from '../constants/url.constants';
 
-export const retrieveCategories = async () => {
-    return await fetch(URL_CONSTANTS.DATABASE_URL + URL_CONSTANTS.CATEGORIES_TABLE)
-    .then((response) => response.json())
-    .then((data) => { return data })
-    .catch(error => console.log(error));
-};
+export const retrieveCategories = async () => await fetch(URL_CONSTANTS.DATABASE_URL + URL_CONSTANTS.CATEGORIES_TABLE)
+  .then((response) => response.json())
+  .then((data) => data)
+  .catch((error) => console.log(error));
 
-export const retrieveVideos = async () => {
-    return await fetch(URL_CONSTANTS.DATABASE_URL + URL_CONSTANTS.VIDEOS_TABLE)
-    .then((response) => response.json())
-    .then((data) => { return data })
-    .catch(error => console.log(error));
-};
+export const retrieveVideos = async () => await fetch(URL_CONSTANTS.DATABASE_URL + URL_CONSTANTS.VIDEOS_TABLE)
+  .then((response) => response.json())
+  .then((data) => data)
+  .catch((error) => console.log(error));
 
 export const saveCategories = async (categories) => {
-    await fetch(URL_CONSTANTS.DATABASE_URL + URL_CONSTANTS.CATEGORIES_TABLE, {
-        method: 'PUT',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(categories)
-    }).catch(error => console.log(error));
+  await fetch(URL_CONSTANTS.DATABASE_URL + URL_CONSTANTS.CATEGORIES_TABLE, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(categories),
+  }).catch((error) => console.log(error));
 };
 
 export const saveVideos = async (videos) => {
-    await fetch(URL_CONSTANTS.DATABASE_URL + URL_CONSTANTS.VIDEOS_TABLE, {
-        method: 'PUT',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(videos)
-    }).catch(error => console.log(error));
+  await fetch(URL_CONSTANTS.DATABASE_URL + URL_CONSTANTS.VIDEOS_TABLE, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(videos),
+  }).catch((error) => console.log(error));
 };
-
